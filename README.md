a4236b6d4295360abcdc0b130e2cd199d562de80
Juan Enciso <juan.enciso@gmail.com>
2023-11-18 11:05:35 +0000

    [FEAT] Add readme

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # aws-ps1
+
+Show your `AWS_PROFILE` environment variable in your prompt terminal
+
+## Usage
+
+clone this repo in your home directory and add these line in your `.bashrc`
+
+```
+source ~/aws-ps1/aws-ps1.sh
+PS1='[\u@\h \W $(aws_ps1)]\$ '
+awson
+export AWS_PROFILE=myaccount
+```
---
+++ aws-ps1.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+AWS_PROFILE_ENABLED="${AWS_PROFILE_ENABLED:-false}"
+AWS_PROFILE_PREFIX="${AWS_PROFILE_PREFIX-[}"
+AWS_PROFILE_SUFFIX="${AWS_PROFILE_SUFFIX-]}"
+AWS_PROFILE_SYMBOL="${AWS_PROFILE_SYMBOL-☁}"
+
+aws_ps1() {
+  if [[ "${AWS_PROFILE_ENABLED}" == true ]] && [[ -n "${AWS_PROFILE}" ]]; then
+    echo "${AWS_PROFILE_PREFIX}${AWS_PROFILE_SYMBOL}${AWS_PROFILE}${AWS_PROFILE_SUFFIX}"
+  fi
+}
+
+awson() {
+  export AWS_PROFILE_ENABLED=true
+}
+
+awsoff() {
+  export AWS_PROFILE_ENABLED=false
+}
